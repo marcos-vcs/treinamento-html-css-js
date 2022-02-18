3700ae427910710cbd761f8ea5a828f6b2616e81
marcos <marcosviniciocardoso123@gmail.com>
2022-02-18 11:55:12 -0300

    improvement -- add delete functionality

--- js/departament.js
+++ js/departament.js
@@ -1,9 +1,10 @@
-function viewtablue()
+function viewtable()
 {
-    var table=document.getElementById("departament-list");
-    const keys = Object.keys(localStorage);
-    var r= keys.length;
 
+    var table=document.getElementById("departament-list");
+    var keys = Object.keys(localStorage);
+    var r= keys.length;
+    
     for(i=0;i<r;i++){
         var row = table.insertRow(i+1);
         var cell1 = row.insertCell(0);
@@ -12,17 +13,44 @@ function viewtablue()
         cell1.innerHTML = keys[i];
         cell2.innerHTML = localStorage.getItem(keys[i]);
         cell3.innerHTML = 
-        '<span onclick="abreModalDelete()" style="font-size: 20px; word-spacing: 10px;">' 
+        `<span onclick="abreModalDelete('deleteModal${keys[i]}');" style="font-size: 20px; word-spacing: 10px;">` 
         + '<i style="cursor:pointer" class="fa-solid fa-pen-to-square"></i>'
         + '&nbsp;'
         + '<i style="cursor:pointer" class="fa-solid fa-trash-alt"></i>'
-        + '</span>';
+        + '</span>' 
+        + `<div id="deleteModal${keys[i]}" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Tem certeza que deseja excluir?</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>A operação que deseja fazer excluí por completo o dado do armazenamento local.</p>
+            </div>
+            <div class="modal-footer">
+              <button onclick="deleterow('${keys[i]}');" data-dismiss="modal" type="button" class="btn btn-primary">Confirmar</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+            </div>
+          </div>
+        </div>
+      </div>`;
+
     }
 
 }
 
-function abreModalDelete() {
-    $("#deleteModal").modal({
+function deleterow(id)
+{
+    localStorage.removeItem(id);
+    location.reload();
+
+}
+
+function abreModalDelete(name) {
+    $("#" + name).modal({
       show: true
     });
 }
