48a08ee918d52f47aa0cccaadf326247f539d30a
marcos <marcosviniciocardoso123@gmail.com>
2022-02-18 10:26:27 -0300

    improvement -- read datas by local storage

---
+++ js/departament.js
@@ -0,0 +1,22 @@
+function viewtablue()
+{
+    var table=document.getElementById("departament-list");
+    const keys = Object.keys(localStorage);
+    var r= keys.length;
+
+    for(i=0;i<r;i++){
+        var row = table.insertRow(i+1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = keys[i];
+        cell2.innerHTML = localStorage.getItem(keys[i]);
+        cell3.innerHTML = 
+        '<span style="font-size: 20px; word-spacing: 10px;">' 
+        + '<i class="fa-solid fa-pen-to-square"></i>'
+        + '&nbsp;'
+        + '<i class="fa-solid fa-trash-alt"></i>'
+        + '</span>';
+    }
+
+}
